030d89f2e5451a31a7034ee6371abd84c8f6c70a
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-11-07 16:46:08 +0200

    adding a new view for payment_details.dart

---
+++ lib/features/checkout/presentation/views/payment_details.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:payment_gateway/core/global_widgets/build_app_bar.dart';
+
+class PaymentDetailsView extends StatelessWidget {
+  const PaymentDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(title: 'Payment Details'),
+      body: const PaymentDetailsViewBody(),
+    );
+  }
+}
+
+class PaymentDetailsViewBody extends StatelessWidget {
+  const PaymentDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
+
