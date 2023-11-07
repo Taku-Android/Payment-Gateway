9c348f45acf35b16250ba31f6b9f03858c186b15
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-11-07 14:55:34 +0200

    adding checkout_app.dart as main page adding presentation layer for one feature payment gateway

---
+++ lib/checkout_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:payment_gateway/features/checkout/presentation/views/my_cart_view.dart';
+
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyCartView(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:payment_gateway/checkout_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CheckoutApp());
 }
+
+
+
 
