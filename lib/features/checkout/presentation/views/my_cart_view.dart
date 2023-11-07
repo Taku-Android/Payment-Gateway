ebbae5029c64c268054fc12270879b725c38a7f0
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-11-07 15:06:02 +0200

    adding appBar and cart body

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -6,7 +6,33 @@ class MyCartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+              "My Cart",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+              )
+          ),
+        ),
+      ),
+      body: const MyCartViewBody(),
     );
   }
 }
+
+class MyCartViewBody extends StatelessWidget {
+  const MyCartViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
+
