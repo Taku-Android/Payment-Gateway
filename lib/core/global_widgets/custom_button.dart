a67d383d5822c8ffeb2de75c2aaa4ffd403f8377
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-11-07 15:50:50 +0200

    add a custom_button.dart to use it in more than one screen

---
+++ lib/core/global_widgets/custom_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../utils/styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.onPressed, required this.title});
+
+  final Function()? onPressed;
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    Color defaultColor =
+    const Color(0xff34A853); // Set your default button color
+    // Color pressedColor = Colors.green[300]!; // Set the color when pressed
+    return ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                (Set<MaterialState> states) {
+              if (states.contains(MaterialState.pressed)) {
+                // The button is pressed
+                return defaultColor;
+              }
+              // The button is not pressed
+              return defaultColor;
+            },
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius:
+              BorderRadius.circular(8), // Custom border radius
+            ),
+          ),
+          elevation: MaterialStateProperty.all<double>(5),
+          // Add elevation to create shadow
+          shadowColor: MaterialStateProperty.all<Color>(
+              Colors.black.withOpacity(0.8)), // Shadow color
+        ),
+        onPressed: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Text(
+            title,
+            style: Styles.style20Grey.copyWith(color: Colors.black),
+          ),
+        ));
+  }
+}
