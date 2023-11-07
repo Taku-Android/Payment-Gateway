8bf1b1fe579e8aaafedb34bacaeea63a008cf529
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-11-07 15:11:50 +0200

    finishing the app styles.dart

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  TextStyle style25Black = const TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+    fontWeight: FontWeight.w500,
+  );
+
+  TextStyle style18Black = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  TextStyle style24Black = const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+
+  TextStyle style20Grey =  TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: Colors.grey[700],
+  );
+
+
+}
