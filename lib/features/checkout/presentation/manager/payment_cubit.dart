ed3552a04b9aa4088516473c4a0881d2ac3ec7d2
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-11-15 02:55:08 +0200

    finishing the payment_cubit.dart

---
+++ lib/features/checkout/presentation/manager/payment_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:payment_gateway/features/checkout/data/model/payment_intent_input_model.dart';
+import 'package:payment_gateway/features/checkout/data/repo/checkout_repository.dart';
+import 'package:payment_gateway/features/checkout/presentation/manager/payment_state.dart';
+
+class PaymentCubit extends Cubit<PaymentState> {
+  PaymentCubit(this.checkOutRepository) : super(PaymentInitial());
+
+  final CheckOutRepository checkOutRepository;
+
+  Future makePayment(PaymentIntentInputModel paymentIntentInputModel) async {
+    emit(PaymentLoading());
+    var response = await checkOutRepository.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel);
+
+    response.fold(
+      (failure) => emit(PaymentFailure(errMessage: failure.errMessage)),
+      (success) => emit(PaymentSuccess()),
+    );
+  }
+}
---
+++ lib/features/checkout/presentation/manager/payment_state.dart
@@ -0,0 +1,14 @@
+
+abstract class PaymentState {}
+
+class PaymentInitial extends PaymentState {}
+
+
+class PaymentLoading extends PaymentState {}
+class PaymentSuccess extends PaymentState {}
+class PaymentFailure extends PaymentState {
+  final String errMessage ;
+
+  PaymentFailure({required this.errMessage});
+
+}
