5f280246d3d78cf2990cd5a90947ad758a095545
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-11-15 02:49:04 +0200

    make a checkout_repository.dart
    make code more clean and use dartz to return either
    failure.dart or null as we don't wait for a return

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,10 @@
+abstract class Failure {
+  final String errMessage ;
+
+  Failure({required this.errMessage});
+}
+
+class ServerError extends Failure{
+  ServerError({required super.errMessage});
+
+}
---
+++ lib/features/checkout/data/repo/checkout_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment_gateway/core/errors/failure.dart';
+import 'package:payment_gateway/features/checkout/data/model/payment_intent_input_model.dart';
+
+abstract class CheckOutRepository{
+
+
+  Future<Either<Failure , void>> makePayment({required PaymentIntentInputModel paymentIntentInputModel});
+
+}
+
