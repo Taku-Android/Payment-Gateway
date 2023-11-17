ec07c9b111c30ca555164d548060bd10e3b22833
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-11-17 05:12:14 +0200

    after finishing stripe payment gateway
    now we add the paypal payment
    and for that i use tharwat samy new package
    and for use it i upgrade the dart sdk and the flutter

--- lib/features/checkout/presentation/widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout/presentation/widgets/custom_button_bloc_consumer.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:payment_gateway/features/checkout/data/model/payment_intent/payment_intent_input_model.dart';
 import 'package:payment_gateway/features/checkout/presentation/manager/payment_cubit.dart';
 import 'package:payment_gateway/features/checkout/presentation/views/thank_you_view.dart';
@@ -32,10 +35,79 @@ class CustomButtonBlocConsumer extends StatelessWidget {
         return CustomButton(
             isLoading: state is PaymentLoading ? true : false,
             onPressed: () {
-              PaymentIntentInputModel paymentIntentInputModel =
-                  PaymentIntentInputModel(amount: '2000', currency: 'USD' , customerId: 'cus_P0w6APTVeYF9kC');
-              BlocProvider.of<PaymentCubit>(context)
-                  .makePayment(paymentIntentInputModel);
+              // PaymentIntentInputModel paymentIntentInputModel =
+              //     PaymentIntentInputModel(amount: '2000', currency: 'USD' , customerId: 'cus_P0w6APTVeYF9kC');
+              // BlocProvider.of<PaymentCubit>(context)
+              //     .makePayment(paymentIntentInputModel);
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => PaypalCheckoutView(
+                  sandboxMode: true,
+                  clientId: "YOUR CLIENT ID",
+                  secretKey: "YOUR SECRET KEY",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": '100',
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": '100',
+                          "shipping": '0',
+                          "shipping_discount": 0
+                        }
+                      },
+                      "description": "The payment transaction description.",
+                      // "payment_options": {
+                      //   "allowed_payment_method":
+                      //       "INSTANT_FUNDING_SOURCE"
+                      // },
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": '10',
+                            "currency": "USD"
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": '12',
+                            "currency": "USD"
+                          }
+                        ],
+
+                        // Optional
+                        //   "shipping_address": {
+                        //     "recipient_name": "Tharwat samy",
+                        //     "line1": "tharwat",
+                        //     "line2": "",
+                        //     "city": "tharwat",
+                        //     "country_code": "EG",
+                        //     "postal_code": "25025",
+                        //     "phone": "+00000000",
+                        //     "state": "ALex"
+                        //  },
+                      }
+                    }
+                  ],
+                  note: "Contact us for any questions on your order.",
+                  onSuccess: (Map params) async {
+                    log("onSuccess: $params");
+                    Navigator.pop(context);
+                  },
+                  onError: (error) {
+                    log("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    print('cancelled:');
+                    Navigator.pop(context);
+                  },
+                ),
+              ));
+
+
+
             },
             title: 'Continue');
       },
